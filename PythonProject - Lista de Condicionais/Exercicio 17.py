72df12367c52abc45942d3bee2644b001ae5fceb
Andre Luis Sousa Dantas <52958920852@>
2025-04-02 15:56:41 -0300

    Correção Ex17

--- PythonProject - Lista de Condicionais/Exercicio 17.py
+++ PythonProject - Lista de Condicionais/Exercicio 17.py
@@ -8,43 +8,29 @@ if valido:
     dia, mes, ano = data.split('/')
     data_invertida = f'{ano}-{mes}-{dia}'
 
-    if (int(ano) % 4 == 0 and int(ano) % 100 != 0) or (int(ano) % 400 == 0):
-        if int(mes) == 2:
-            if int(dia) >= 1 or int(dia) <= 29:
-                print(f'A data formatada é: {data_invertida}')
-            else:
-                print("Dia invalida")
-        elif int(mes) == 1 or int(mes) == 3 or int(mes) == 5 or int(mes) == 7 or int(mes) == 8 or int(mes) == 10 or int(
-                mes) == 12:
-            if int(dia) >= 1 or int(dia) <= 31:
-                print(f'A data formatada é: {data_invertida}')
-            else:
-                print("Dia invalida")
-        elif int(mes) == 4 or int(mes) == 6 or int(mes) == 9 or int(mes) == 11:
-            if int(dia) >= 1 or int(dia) <= 30:
+    if int(mes) == 2:
+        if (int(ano) % 4 == 0 and int(ano) % 100 != 0) or (int(ano) % 400 == 0):
+            if int(dia) >= 1 and int(dia) <= 29:
                 print(f'A data formatada é: {data_invertida}')
             else:
                 print("Dia invalida")
         else:
-            print("Mês invalido")
+            if int(dia) >= 1 and int(dia) <= 28:
+                print(f'A data formatada é: {data_invertida}')
+            else:
+                print("Dia invalida")
+    elif int(mes) == 1 or int(mes) == 3 or int(mes) == 5 or int(mes) == 7 or int(mes) == 8 or int(mes) == 10 or int(
+            mes) == 12:
+        if int(dia) >= 1 and int(dia) <= 31:
+            print(f'A data formatada é: {data_invertida}')
+        else:
+            print("Dia invalida")
+    elif int(mes) == 4 or int(mes) == 6 or int(mes) == 9 or int(mes) == 11:
+        if int(dia) >= 1 and int(dia) <= 30:
+            print(f'A data formatada é: {data_invertida}')
+        else:
+            print("Dia invalida")
     else:
-        if int(mes) == 2:
-            if int(dia) >= 1 or int(dia) <= 2:
-                print(f'A data formatada é: {data_invertida}')
-            else:
-                print("Dia invalida")
-        elif int(mes) == 1 or int(mes) == 3 or int(mes) == 5 or int(mes) == 7 or int(mes) == 8 or int(mes) == 10 or int(
-                mes) == 12:
-            if int(dia) >= 1 or int(dia) <= 31:
-                print(f'A data formatada é: {data_invertida}')
-            else:
-                print("Dia invalida")
-        elif int(mes) == 4 or int(mes) == 6 or int(mes) == 9 or int(mes) == 11:
-            if int(dia) >= 1 or int(dia) <= 30:
-                print(f'A data formatada é: {data_invertida}')
-            else:
-                print("Dia invalida")
-        else:
-            print("Mês invalido")
+        print("Mês invalido")
 else:
     print("Escrito incorretamente")
